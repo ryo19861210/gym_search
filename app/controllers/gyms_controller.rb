aa48611e4db589c0dd5270c87df2b722bff7f701
ryo19861210 <superdio3633hornetkkauemi1210@gmail.com>
2022-07-21 00:25:59 +0900

    commit

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -1,7 +1,6 @@
 class GymsController < ApplicationController
   def index
     @gyms = Gym.all
-    @team = Team.new(team_params)
   end
 
   def show
@@ -12,11 +11,5 @@ class GymsController < ApplicationController
 
   def search
     @gyms = Gym.search(params[:keyword])
-  end
-
-  private
-
-  def team_params
-    params.require(:team).permit(:customer_id, :active_until, :stripe_subscription_id, :plan_id).merge(user_id: current_user.id)
   end
 end
